0197c35cfafbc0d71e71e971fda92bbdbf8af368
Gretchen <gretchonian@hotmail.com>
2017-06-16 21:25:57 +0000

    Added Enrollment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
   has_many :courses
+  has_many :enrollments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def enrolled_in?(course)
+    enrolled_courses = enrollments.collect do |enrollment|
+      enrollment.course
+    end
+    return enrolled_courses.include?(course)
+  end
 end
